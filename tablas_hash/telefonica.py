ef7fae746e9b0b9ca45ff0fcdd2cd6cb7d63ebfe
davidprz01 <naranjo0130@gmail.com>
2025-03-29 12:45:11 -0500

    Agregando nuevos cambios de estructura

--- tablas_hash/telefonica.py
+++ tablas_hash/telefonica.py
@@ -28,10 +28,11 @@ class Hashtable:
                 if current.key == key:
                     current.value = value
                     return
+                if current.next is None:
+                    break
+
                 current = current.next
-            newnode = Node(key,value)
-            newnode.next = self.table[index]
-            self.table[index] = newnode
+            current.next = Node(key, value)
     
     def search(self, key):
         index = self._hash(key)
@@ -41,17 +42,17 @@ class Hashtable:
             if current.key == key:
                 return current.value
             current = current.next
-            return print("no se ha encontrado el elemento")
+        return print("no se ha encontrado el elemento")
         
 
-sistema = Hashtable(5)
+tablehash = Hashtable(5)
 
-sistema.insert("Nombre", "David")
-sistema.insert("Telefono","304-6528-9433")
+tablehash.insert("Nombre", "David")
+tablehash.insert("Telefono","304-6528-9433")
 
-print(sistema.search("Nombre"))
-print(sistema.search("Telefono"))
-print(sistema._hash("Nombre"))
+print(tablehash.search("Nombre"))
+print(tablehash.search("Telefono"))
+print(tablehash._hash("Nombre"))
 
     
 
